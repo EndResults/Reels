bfa37e18ae9e37e0a38b001abbc0d0406f6ee236
EndResults <clinton@endresults.nl>
2025-11-26 21:51:50 +0100

    Initial load v3

--- src/server/dist/lib/fileUpload.d.ts
+++
@@ -1,5 +0,0 @@
-import multer from 'multer';
-export declare const upload: multer.Multer;
-export declare const getFileUrl: (filePath: string) => string;
-export declare const deleteFile: (filePath: string) => void;
-//# sourceMappingURL=fileUpload.d.ts.map
--- src/server/dist/services/scrapeLogger.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ExtractResult } from './adaptiveScraper';
-export declare function logScrape(res: ExtractResult, loadTime: number, statusCode?: number): Promise<void>;
-//# sourceMappingURL=scrapeLogger.d.ts.map
